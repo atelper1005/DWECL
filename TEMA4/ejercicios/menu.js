62061cd25f3da86a70d475c507506c17b6fe86ab
unknown <you@example.com>
2023-09-29 10:32:54 +0200

    actualizacion ejercicio tema 4

--- TEMA4/ejercicios/menu.js
+++ TEMA4/ejercicios/menu.js
@@ -1,9 +1,87 @@
-alert("");
-switch (key) {
-    case value:
-        
-        break;
+function mostrarMenu() {
+    var opcion = Number(prompt("Elija una opción: \n1. Elegir figura geométrica \n2. Ingresar medidas \n3. Calcular volumen \n4. Calcular área \n5. Finalizar programa"));
 
-    default:
-        break;
+    if (opcion === null) {
+        return;
+    }
+
+    switch (opcion) {
+        case 1:
+            elegirFigura();
+            break;
+
+        case 2:
+            ingresarMedidas();
+            break;
+
+        case 3:
+            calcularVolumen();
+            break;
+
+        case 4:
+            calcularArea();
+            break;
+
+        case 5:
+            alert("Programa finalizado");
+            break;
+
+        default:
+            alert("Opción no valida, elija de nuevo");
+            mostrarMenu();
+            break;
+    }
+}
+
+function elegirFigura() {
+    var figura = Number(prompt("Elija una figura geométrica: \n1. Cubo \n2. Esfera \n3. Cilindro"));
+
+    if(figura === null) {
+        alert("Opción no valida, elija de nuevo");
+        mostrarMenu();
+        return;
+    } else if(figura === 1) {
+        alert("Cubo seleccionado")
+        
+    } else if(figura === 2) {
+        alert("Esfera seleccionada")
+        
+    } else if(figura === 3) {
+        alert("Cilindro seleccionado")
+        
+    } else {
+        alert("Opción no válida, elija de nuevo")
+        return elegirFigura();
+    }
+
+    mostrarMenu();
+}
+
+function ingresarMedidas() {
+
+    if(elegirFigura() == 1) {
+        prompt("Ingrese el lado del cubo (en cm): ");
+        
+    } else if (figuraElegida == 2) {
+        prompt("Ingrese el radio de la esfera (en cm): ");
+        
+    } else if (figuraElegida == 3) {
+        prompt("Ingrese radio del cilindro (en cm)");
+        prompt("Ingrese la altura del cilindro (en cm): ");
+        
+    }
+
+    mostrarMenu();
+}
+
+function calcularVolumen() {
+    // utilizando las medidas almacenadas previamente y lo mostraríamos en una ventana emergente
+
+    mostrarMenu();
+}
+
+function calcularArea() {
+    // utilizando las medidas almacenadas previamente y lo mostraríamos en una ventana emergente
+
+    mostrarMenu();
 }
