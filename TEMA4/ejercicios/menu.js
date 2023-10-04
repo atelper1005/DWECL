fbf4215b578dde44eea473f7d2df00909e3c94fc
unknown <you@example.com>
2023-10-04 11:26:41 +0200

    proyecto terminado

--- TEMA4/ejercicios/menu.js
+++ TEMA4/ejercicios/menu.js
@@ -1,100 +1,116 @@
-function() {
 function mostrarMenu() {
-    var opcion = Number(prompt("Elija una opción: \n1. Elegir figura geométrica \n2. Ingresar medidas \n3. Calcular volumen \n4. Calcular área \n5. Finalizar programa"));
-    var figura;
-    var dimensiones;
+    var opcion = 0;
 
-    function elegirFigura() {
-        figura = prompt("Elige una figura geométrica: cubo, esfera, cilindro");
-    }
-    
-    function ingresarMedidas() {
-        switch (figura) {
-            case 'cubo':
-                dimensiones = prompt("Introduce la longitud del lado del cubo");
-                break;
-            case 'esfera':
-                dimensiones = prompt("Introduce el radio de la esfera");
-                break;
-            case 'cilindro':
-                dimensiones = prompt("Introduce el radio y la altura del cilindro, separados por una coma");
-                break;
-            default:
-                alert("Figura no reconocida");
-                f1();
-        }
-    }
+				var figura = 0;
+				var lado = 0;
+				var radio = 0;
+				var altura = 0;
+				var area = 0;
+                var vol = 0;
 
-     function calcularVolumen() {
-        let volumen;
-        switch (figura) {
-            case 'cubo':
-                volumen = Math.pow(dimensiones, 3);
-                break;
-            case 'esfera':
-                volumen = (4/3) * Math.PI * Math.pow(dimensiones, 3);
-                break;
-            case 'cilindro':
-                let [radio, altura] = dimensiones.split(',').map(Number);
-                volumen = Math.PI * Math.pow(radio, 2) * altura;
-                break;
-        }
-        alert(`El volumen de la ${figura} es ${volumen}`);
-    }
+				while (true) {
+					opcion = Number(
+						prompt(
+							"1 : Elige una figura entre Cubo, Esfera, y Cilindro\n" +
+								"2 : Introduzca los parámetros necesarios\n" +
+								"3 : Calcular Volumen\n" +
+								"4 : Calcular Área\n" +
+								"5 : Finalizar programa"
+						)
+					);
 
-    function calcularArea() {
-        let area;
-        switch (figura) {
-            case 'cubo':
-                area = 6 * Math.pow(dimensiones, 2);
-                break;
-            case 'esfera':
-                area = 4 * Math.PI * Math.pow(dimensiones, 2);
-                break;
-            case 'cilindro':
-                let [radio, altura] = dimensiones.split(',').map(Number);
-                area = 2 * Math.PI * radio * (radio + altura);
-                break;
-        }
-        alert(`El área de la ${figura} es ${area}`);
-    }
+					switch (opcion) {
+						case 1:
+							figura = elegirFigura();
+							alert("Has elegido " + figura);
+							break;
 
-    if (opcion === null) {
-        return;
-    }
+						case 2:
+							if (typeof figura === "undefined") {
+								alert("Debes elegir una figura primero.");
+							} else if (figura == 1) {
+								lado = introducirValores(figura);
+								alert(lado);
+							} else if (figura == 2) {
+								radio = introducirValores(figura);
+								alert(radio);
+							} else if (figura == 3) {
+								[radio, altura] = introducirValores(figura);
+								alert(radio);
+								alert(altura);
+							}
+							break;
 
-    switch (opcion) {
-        case '1':
-            elegirFigura();
-            mostrarMenu();
-            break;
+						case 3:
+							vol = calcularVolumen(figura, lado, radio, altura);
+							alert(vol);
+							break;
 
-        case '2':
-            ingresarMedidas();
-            mostrarMenu();
-            break;
+						case 4:
+							area = calcularArea(figura, lado, radio, altura);
+							alert(area);
+							break;
 
-        case '3':
-            calcularVolumen();
-            mostrarMenu();
-            break;
+						case 5:
+                            alert("Programa finalizado")
+							break;
 
-        case '4':
-            calcularArea();
-            mostrarMenu();
-            break;
+						default:
+							alert("Opción no válida. Por favor, elige una opción válida.");
+					}
+				}
+			}
 
-        case '5':
-            alert("Programa finalizado");
-            break;
+			function elegirFigura() {
+				var figura = Number(
+					prompt("1 : Cubo\n" + "2 : Esfera\n" + "3 : Cilindro")
+				);
+				return figura;
+			}
 
-        default:
-            alert("Opción no valida, elija de nuevo");
-            mostrarMenu();
-            break;
-    }
-}
-    
-}
+			function introducirValores(figura) {
+				if (figura == 1) {
+					var lado = Number(prompt("Introduzca el Lado del Cubo: "));
+					return lado;
+				} else if (figura == 2) {
+					var radio = Number(prompt("Introduzca el Radio de la Esfera: "));
+					return radio;
+				} else if (figura == 3) {
+					var radio = Number(prompt("Introduzca el Radio del Cilindro: "));
+					var altura = Number(prompt("Introduzca la Altura del Cilindroo: "));
+					return [radio, altura];
+				} else {
+					alert("Datos Incorrectos");
+				}
+			}
 
-mostrarMenu();
+			function calcularVolumen(figura, lado, radio, altura) {
+				if (figura == 1) {
+					// Cálculo del volumen del cubo
+                    var vol = (Math.pow(lado, 3));
+				} else if (figura == 2) {
+					// Cálculo del volumen de la esfera
+                    var vol = (4/3) * Math.PI * (Math.pow(radio, 3));
+				} else if (figura == 3) {
+					// Cálculo del volumen del cilindro
+                    var vol = Math.PI * (Math.pow(radio, 2)) * altura;
+				}
+                return vol;
+			}
+
+			function calcularArea(figura, lado, radio, altura) {
+				if (figura == 1) {
+                    // Cálculo del area del cubo
+					var area = 6 * Math.pow(lado, 2); 
+				} else if (figura == 2) {
+                    // Cálculo del area de la esfera
+					var area = 4 * Math.PI * Math.pow(radio, 2); 
+				} else if (figura == 3) {
+                    // Cálculo del area del cilindro
+					var area = 2 * Math.PI * radio * (radio + altura); 
+				}
+                return area;
+			}
+            
+
+			mostrarMenu();
