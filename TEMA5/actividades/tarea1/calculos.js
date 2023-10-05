8a8beae19c84ca4009b1d8a6df24b5e6ceef1661
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2023-10-05 17:43:30 +0200

    Update calculos.js

--- TEMA5/actividades/tarea1/calculos.js
+++ TEMA5/actividades/tarea1/calculos.js
@@ -9,25 +9,68 @@
 
 
 
-function calcularMedia() {
-    var suma = valores
-}
+        function calcularResultados() {
+            // Obtén los números ingresados por el usuario
+            var numerosTexto =  prompt("Ingresa números separados por comas:");
 
-function calcularRango() {
+            // Convierte los números de texto a un array de números
+            var numeros = numerosTexto.split(',').map(function(num) {
+                return parseFloat(num);
+            });
 
-}
+            // Filtra los elementos no numéricos
+            numeros = numeros.filter(function(num) {
+                return !isNaN(num);
+            });
 
-function calcularModa() {
+            if (numeros.length === 0) {
+                alert("Por favor, ingresa números válidos.");
+                return;
+            }
 
-}
+            // Calcula la media
+            var media = alert("La media es " + calcularMedia(numeros));
 
-function anadirValores() {
-    var valores = [];
-    var num = 0;
+            // Calcula el rango
+            var rango = alert("El rango es " + calcularRango(numeros));
 
-    for (var i = 0; !isNaN(num) && num != null; i++) {
-        num = Number(prompt("Añada valores numéricos " + "\nValores añadidos: " + i + " \n(Si desea dejar de añadir valores pulse cualquier valor no numérico)"));
-        valores [i] = num;
-    }
-    return valores;
-}
+            // Calcula la moda
+            var moda = alert("La moda es " + calcularModa(numeros));
+
+        }
+
+        function calcularMedia(numeros) {
+            var suma = numeros.reduce(function(acc, num) {
+                return acc + num;
+            }, 0);
+            return suma / numeros.length;
+        }
+
+        function calcularRango(numeros) {
+            var max = Math.max.apply(null, numeros);
+            var min = Math.min.apply(null, numeros);
+            return max - min;
+        }
+
+        function calcularModa(numeros) {
+            var conteo = {};
+            var moda = [];
+            var maxFrecuencia = 0;
+
+            // Contar la frecuencia de cada número
+            numeros.forEach(function(num) {
+                conteo[num] = (conteo[num] || 0) + 1;
+                if (conteo[num] > maxFrecuencia) {
+                    maxFrecuencia = conteo[num];
+                }
+            });
+
+            // Encontrar los números con la frecuencia máxima (puede haber múltiples)
+            for (var num in conteo) {
+                if (conteo.hasOwnProperty(num) && conteo[num] === maxFrecuencia) {
+                    moda.push(parseFloat(num));
+                }
+            }
+
+            return moda;
+        }
