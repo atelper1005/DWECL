00bada665282f9f0352862f614b6b808cbf2d4d5
unknown <atelper1005@g.educaand.es>
2023-10-19 10:18:12 +0200

    ejercicios

---
+++ TEMA6/actividades/ParteA/02/arrays.js
@@ -0,0 +1,26 @@
+function recogerDatos() {
+    var matriz = [];
+    var fila = [];
+    var numIntro = 0;
+    var conteo = 0;
+    
+    while(true) {
+      numIntro = prompt("Introduzca un valor" + "\nFilas actuales: " + conteo + "\n Pulse c para añadir otra fila" + "\n Pulse f para salir")
+
+      
+    }
+}
+
+function separarArrays() {
+    var pares = [];
+    var impares = [];
+
+    if(matriz === 0 || matriz === null) {
+      
+    }
+}
+
+function mostrarArrays() {
+  alert("Números pares: " + pares + "\nNúmeros impares: " + impares);
+}
+
