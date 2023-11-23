c12d2d4e3a8756f1339b0f0c2aa002f6bfe5802f
atelper1005 <atelper1005@g.educaand.es>
2023-11-23 10:34:41 +0100

    Tarea1 Tema8 v2

--- TEMA8/ejercicios/codigo.js
+++ TEMA8/ejercicios/codigo.js
@@ -10,51 +10,158 @@ class Producto {
 }
 
 // Estructura del carrito
-class Carrito {
+class Tienda {
   constructor() {
-    this.productos = [];
+    this.baseDeDatos = [
+      new Producto(1, "Zapatos", 45, "https://loremflickr.com/320/240/shoes"),
+      new Producto(2, "Móvil", 99.99, "https://loremflickr.com/320/240/smartphone"),
+      new Producto(3, "Vestido", 30, "https://loremflickr.com/320/240/dress"),
+      new Producto(4, "Ratón", 15.5, "https://loremflickr.com/320/240/wireless_mouse"),
+      new Producto(5, "Maceta", 27.9, "https://loremflickr.com/320/240/flowerpot"),
+      new Producto(6, "Collar", 50, "https://loremflickr.com/320/240/necklace")
+    ];
+
+  this.carrito = [];
+  this.divisa = '€';
+  this.DOMitems = document.querySelector('#items');
+  this.DOMcarrito = document.querySelector('#carrito');
+  this.DOMtotal = document.querySelector('#total');
+  this.DOMbotonVaciar = document.querySelector('#boton-vaciar');
   }
 
-  // Método para agregar productos al carrito
-  agregarProducto(producto) {
-    this.productos.push(producto);
-  }
-
-  // Método para calcular el total
-  calcularTotal() {
-    let total = 0;
-    this.productos.forEach((producto) => {
-      total += producto.precio * producto.cantidad;
+  renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+        // Boton
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        // Insertamos
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
     });
-    return total;
-  }
-
-  // Método para calcular el total con IVA
-  calcularTotalConIva() {
-    return this.calcularTotal() * 1.21;
-  }
 }
 
-//BBDD de los productos de la tienda
-var productos = [
-  [1, "Zapatos", 45, "https://loremflickr.com/320/240/shoes"],
-  [2, "Móvil", 99.99, "https://loremflickr.com/320/240/smartphone"],
-  [3, "Vestido", 30, "https://loremflickr.com/320/240/dress"],
-  [4, "Ratón", 15.5, "https://loremflickr.com/320/240/wireless_mouse"],
-  [5, "Maceta", 27.9, "https://loremflickr.com/320/240/flowerpot"],
-  [6, "Collar", 50, "https://loremflickr.com/320/240/necklace"],
-];
-// Crear carrito
-var carrito = new Carrito();
+/**
+* Evento para añadir un producto al carrito de la compra
+*/
+anyadirProductoAlCarrito(evento) {
+    // Anyadimos el Nodo a nuestro carrito
+    carrito.push(evento.target.getAttribute('marcador'))
+    // Actualizamos el carrito
+    renderizarCarrito();
 
-// Añadir productos al carrito
-document.getElementById("catalogo").addEventListener("click", (event) => {
-  if (event.target.classList.contains("agregar-producto")) {
-    const id = event.target.dataset.id;
-    const producto = productosDisponibles.find(
-      (producto) => producto.id === parseInt(id)
-    );
-    carrito.agregarProducto(producto);
-    mostrarCarrito();
-  }
+}
+
+/**
+* Dibuja todos los productos guardados en el carrito
+*/
+renderizarCarrito() {
+    // Vaciamos todo el html
+    DOMcarrito.textContent = '';
+    // Quitamos los duplicados
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Generamos los Nodos a partir de carrito
+    carritoSinDuplicados.forEach((item) => {
+        // Obtenemos el item que necesitamos de la variable base de datos
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            // ¿Coincide las id? Solo puede existir un caso
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Cuenta el número de veces que se repite el producto
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        // Creamos el nodo del item del carrito
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio.toFixed(2)}${divisa} | Subtotal = ${(numeroUnidadesItem*miItem[0].precio).toFixed(2)}${divisa}`;
+        // Boton de borrar
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        // Mezclamos nodos
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+   // Renderizamos el precio total en el HTML
+   DOMtotal.textContent = calcularTotal();
+}
+
+/**
+* Evento para borrar un elemento del carrito
+*/
+borrarItemCarrito(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Borramos todos los productos
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    // volvemos a renderizar
+    renderizarCarrito();
+}
+
+/**
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+calcularTotal() {
+    // Recorremos el array del carrito
+    return carrito.reduce((total, item) => {
+        // De cada elemento obtenemos su precio
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumamos al total
+        return total + miItem[0].precio * 1.21;
+    }, 0).toFixed(2);
+}
+
+/**
+* Varia el carrito y vuelve a dibujarlo
+*/
+vaciarCarrito() {
+    // Limpiamos los productos guardados
+    carrito = [];
+    // Renderizamos los cambios
+    renderizarCarrito();
+}
+
+iniciar() {
+  this.renderizarProductos();
+  this.renderizarCarrito();
+}
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tienda = new Tienda();
+  tienda.iniciar();
 });
