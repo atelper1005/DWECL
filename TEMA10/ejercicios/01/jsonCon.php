e754ba10ec8086603edc6a87519f898ad08bd8b2
atelper1005 <atelper1005@g.educaand.es>
2024-01-17 12:07:45 +0100

    Cambios finales - ejercicio 1

--- TEMA10/ejercicios/01/jsonCon.php
+++ TEMA10/ejercicios/01/jsonCon.php
@@ -2,24 +2,45 @@
 class Conexion
 {
     public $pdo;
+
     public function __construct()
     {
         try {
             $dsn = "mysql:host=localhost;dbname=tema10";
-            $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_PERSISTENT => false,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
-            ];
-            $this->pdo = new PDO($dsn, "root", "", $options);
+            $this->pdo = new PDO($dsn, "root", "", []);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_PERSISTENT, false);
+            $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->pdo->exec("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
         } catch (PDOException $e) {
-            exit();
+            error_log("Error al crear la conexión: " . $e->getMessage());
+            echo "Ha ocurrido un error al conectarse a la base de datos.";
+            exit;
         }
     }
 }
 
-if (!isset($_GET['ClienteId'])) {
+if (isset($_GET['datos'])) {
+    // Vamos a crear la consulta correspondiente
+    $id = $_GET['datos'];
+    $sql = "SELECT id, nombre, apellidos, ciudad FROM tema10.datos WHERE id = :id";
+
+    // Realizamos la conexión
+    $conexion = new Conexion();
+    $pdostmt = $conexion->pdo->prepare($sql);
+
+    // Vinculamos la variable
+    $pdostmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+    // Ejecutamos la consulta
+    $pdostmt->execute();
+
+    // Indicamos el tipo de fetch para el resultado
+    $result = $pdostmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Devolvemos el resultado, convertido a JSON
+    echo json_encode($result);
+} else {
     // Vamos a crear la consulta correspondiente
     $sql = "SELECT id, nombre, apellidos, ciudad FROM tema10.datos";
 
@@ -35,25 +56,5 @@ if (!isset($_GET['ClienteId'])) {
 
     // Devolvemos el resultado, convertido a JSON
     echo json_encode($result);
-} else {
-    // Vamos a crear la consulta correspondiente
-    $sql = "SELECT id, nombre FROM tema10.datos";
-
-    // Realizamos la conexión
-    $conexion = new Conexion();
-    $pdostmt = $conexion->pdo->prepare($sql);
-
-    // Vinculamos la variable
-    // $pdostmt->bindParam(':id', $id, PDO::PARAM_INT);
-
-    // Ejecutamos la consulta
-    $pdostmt->execute();
-
-    // Indicamos el tipo de fetch para el resultado
-    $result = $pdostmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // Devolvemos el resultado, convertido a JSON
-    echo json_encode($result);
 }
-
 ?>
