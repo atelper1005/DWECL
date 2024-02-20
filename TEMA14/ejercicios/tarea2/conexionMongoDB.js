4e31b6b9af8080f9204d249187dfcab1754a0159
atelper1005 <atelper1005@g.educaand.es>
2024-02-20 12:36:06 +0100

    falta conexion

--- TEMA14/ejercicios/tarea2/conexionMongoDB.js
+++ TEMA14/ejercicios/tarea2/conexionMongoDB.js
@@ -3,89 +3,49 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const PORT = 3000;
 
-// Conexion URI
 const uri = "mongodb+srv://atelper1005:atelper1005@tarea2.ihhkf1d.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-//Solucitudes JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-async function run() {
-    const client = new MongoClient(uri);
-  try {
-    await client.connect();
-        console.log("Conexión exitosa a la base de datos");
+const router = express.Router();
+var __dirname = path.resolve();
 
-        // Devolver la instancia del cliente y la base de datos
-        return {
-            client,
-            database: client.db('tarea2'),
-            collection: client.db('tarea2').collection('personas')
-        };
-    } catch (error) {
-        console.error("Error al conectar a la base de datos:", error);
-        throw error;
-  }
-}
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-run().catch(console.dir);
+// Obtiene por POST un JSON con los datos de los input y los introduce en la base de datos
+app.post("/addDatos", async (req, res) => {
+    try {
+        const { nombre, apellidos } = req.body;
+        const db = client.db();
+        const collection = db.collection("personas");
+        await collection.insertOne({ nombre, apellidos });
+        res.send("Documento añadido correctamente");
+    } catch (err) {
+        console.error("Error: ", err);
+        res.send("Error al añadir documento");
+    }
+});
 
-// // Script.js
+// Obtiene por GET como un JSON todos los datos que tiene actualmente la base de datos
+app.get("/showPersonas", async (req, res) => {
+    try {
+        const db = client.db();
+        const collection = db.collection("datos");
+        const documents = await collection.find({}).toArray();
+        res.json(documents);
+    } catch (err) {
+        console.error("Error: ", err);
+        res.send("Error al obtener documentos");
+    }
+});
 
-// // we create 'users' collection in newdb database
-// var url = "mongodb://localhost:27017/newdb";
- 
-// // create a client to mongodb
-// var MongoClient = require('mongodb').MongoClient;
- 
-// // make client connect to mongo service
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     // db pointing to newdb
-//     console.log("Switched to "+db.databaseName+" database");
-//     // create 'users' collection in newdb database
-//     db.createCollection("users", function(err, result) {
-//         if (err) throw err;
-//         console.log("Collection is created!");
-//         // close the connection to db when you are done with it
-//         db.close();
-//     });
-// });
+app.use("/", router);
+app.use(express.static(__dirname));
 
-// var express = require('express');
-// var app = express();
-// var MongoClient = require('mongodb').MongoClient;
-// var url = 'mongodb://localhost/EmployeeDB';
-// var str = "";
-
-// app.route('/Employeeid').get(function(req, res) {
-//    MongoClient.connect(url, function(err, db) {
-//        var collection = db.collection('Employee');
-//        var cursor = collection.find({});
-//        str = "";
-//        cursor.forEach(function(item) {
-//            if (item != null) {
-//                     str = str + "    Employee id  " + item.Employeeid + "</br>";
-//                 }
-//        }, function(err) {
-//            res.send(err);
-//            db.close();
-//           }
-//        );
-//    });
-// });
-// var server = app.listen(8080, function() {});
-
-//Insertar registros
-// var MongoClient = require('mongodb').MongoClient;
-// var url = 'mongodb://localhost/EmployeeDB';
-
-// MongoClient.connect(url, function(err, db) {
-
-//     db.collection('Employee').insertOne({
-//         Employeeid: 4,
-//         EmployeeName: "NewEmployee"
-//     });
-// }); 
+app.listen(3000, () => {
+    console.log("Escuchando en puerto 3000");
+});
